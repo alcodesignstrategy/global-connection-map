45952477de1ff15fb158d75cccf0cd52f8ba8746
alcodesignstrategy <160197404+alcodesignstrategy@users.noreply.github.com>
2024-02-23 04:54:55 +0000

    fixed typo

--- pages/0_Hi.py
+++ pages/0_Hi.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.graph_objects as go
 
 st.write(
-    """This shows your connections!"""
+    """This shows your connections!""")
 
 # Step 1: Load the Excel file
 uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx"])
