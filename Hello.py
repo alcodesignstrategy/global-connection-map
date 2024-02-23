9973a5021743a5754f21585ca483865cabac83a9
alcodesignstrategy <160197404+alcodesignstrategy@users.noreply.github.com>
2024-02-23 03:58:10 +0000

    Added a map

--- Hello.py
+++ Hello.py
@@ -1,51 +1,65 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import pandas as pd
+import pydeck as pdk
 
-LOGGER = get_logger(__name__)
+# Streamlit interface for file upload
+uploaded_file = st.file_uploader("Choose a file", type=['xlsx'])
 
+if uploaded_file is not None:
+    # Read data from the uploaded file
+    df = pd.read_excel(uploaded_file, engine='openpyxl')
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+    # Define color mapping for categories
+    color_map = {
+        'Business': [0, 128, 0],  # Green
+        'Research and Design': [64, 224, 208],  # Turquoise
+        'Technology': [0, 0, 255]  # Blue
+    }
+
+    # Prepare data for map
+    data_for_map = df.copy()
+    data_for_map['color'] = data_for_map['Category'].map(color_map)
+
+    # Streamlit dashboard title
+    st.title("Connections Map")
+
+    # Define the initial view state of the map
+    view_state = pdk.ViewState(latitude=0, longitude=-60, zoom=1)
+
+    # Prepare the scatter plot layer
+    scatterplot_layer = pdk.Layer(
+        "ScatterplotLayer",
+        data_for_map,
+        get_position=["Longitude", "Latitude"],
+        get_color="color",
+        get_radius=200000,
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    # Prepare lines for connections
+    lines_data = []
+    for index, row in df.iterrows():
+        if pd.notna(row['Other Connection ID']):
+            other = df[df['ID Number'] == row['Other Connection ID']].iloc[0]
+            lines_data.append({
+                'source': [row['Longitude'], row['Latitude']],
+                'target': [other['Longitude'], other['Latitude']],
+                'color': [255, 165, 0]  # Orange
+            })
 
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+    line_layer = pdk.Layer(
+        type="LineLayer",
+        data=lines_data,
+        get_source_position="source",
+        get_target_position="target",
+        get_color="color",
+        get_width=5,
     )
 
-
-if __name__ == "__main__":
-    run()
+    # Render the map
+    st.pydeck_chart(pdk.Deck(
+        map_style="mapbox://styles/mapbox/light-v9",
+        initial_view_state=view_state,
+        layers=[scatterplot_layer, line_layer],
+    ))
+else:
+    st.text("Please upload an Excel file to get started.")
